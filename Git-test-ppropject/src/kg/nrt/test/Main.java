95ef4ce8235e60c32437f8e1a8cd6905cdf0c771
ntokbaev <nurlant30@gmail.com>
2023-06-22 12:04:06 +0600

    add 2 !

--- Git-test-ppropject/src/kg/nrt/test/Main.java
+++ Git-test-ppropject/src/kg/nrt/test/Main.java
@@ -3,7 +3,7 @@ package kg.nrt.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from GitHub!");
+		System.out.println("Hello from GitHub!!!");
 
 	}
 
